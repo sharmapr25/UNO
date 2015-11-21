a3dbcaee102802ca450dfb92f7cf852c8a6eb33b
Arindam Karmakar <arindam@INarindam.local>
2015-11-21 19:42:29 +0530

    [Arindam] all tests in 'discardPileTest.js' is added and tested

--- test/discardPileTest.js
+++ test/discardPileTest.js
@@ -1,21 +1,21 @@
-var discard_pile = require('../entities.js').DiscardPile;
+var discard_pile = require('../cardEntities.js').DiscardPile;
 var lodash = require('lodash');
 var assert = require('chai').assert;
 var expect = require('chai').expect;
 
 //DO NOT WRITE ANYTHING OUTSIDE THE TESTING FRAMEWORK..CODE NEEDS TO BE TESTED..KINDLY MOVE IT INSIDE..
 
-// var sampleCards = [{color:'red',number:1,speciality:null},
-// 				{color:null,number:null,speciality:'wildDrawFour'},
-// 				{color:'yellow',number:null,speciality:'reverse'}]
+var sampleCards = [{color:'red',number:1,speciality:null},
+				{color:null,number:null,speciality:'wildDrawFour'},
+				{color:'yellow',number:null,speciality:'reverse'}]
 
-// var discardPile = new discard_pile(sampleCards);
+var discardPile = new discard_pile(sampleCards);
 
-// var fieldsOfDiscardCard=function(discardCards){
-// 	return discardCards.cards.filter(function(card){
-// 		return Object.keys(card)==['color','number','speciality']
-// 	})
-// }
+var fieldsOfDiscardCard=function(discardCards){
+	return discardCards.cards.filter(function(card){
+		return Object.keys(card)==['color','number','speciality']
+	})
+}
 
 describe('discardPile', function(){
 	it('should be a contructor', function(){
