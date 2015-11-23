3246e9fbb46229ea4c16e506ed2dcd55c0eedc7e
Arindam Karmakar <arindam@INarindam.local>
2015-11-23 18:29:20 +0530

    [Arindam & preeti] (playerEntities.js & initalizePlayersTest.js) code for playerEntities is written and made the tests passed

--- playerEntities.js
+++ playerEntities.js
@@ -1,24 +1,54 @@
-var InitializePlayers = function(allPlayers){
+var InitializePlayers = function(allPlayers,direction){
 	this.players = allPlayers;
+	this.direction = direction;
+
+	Object.defineProperties(this,{
+		currentPlayer : {value : this.players[0],writable : true},
+		nextPlayer : {value : this.players[1],writable : true},
+		previousPlayer : {value : this.players[this.players.length - 1],writable : true}
+	})
 };
 
+var getElementIndex = function (allPlayers,currentPlayer,direction) {
+	var index = allPlayers.indexOf(currentPlayer);
+	if(direction){
+		if ((index+1) == allPlayers.length) {
+			return index = 0;
+		};
+		return index+1;
+	}else{
+		if ((index-1)<0) {
+			return index = allPlayers.length-1;
+		};
+		return index-1;
+	}
+}
+
 InitializePlayers.prototype = {
-	currentPlayer : function () {
-		return this.players[0];
-	},
-	nextPlayer : function () {
-		return this.players[1]
-	},
-	previousPlayer : function () {
-		return this.players[this.players.length - 1];
-	},
 	changePlayersTurn : function () {
-		var recentPlayer = this.players.shift();
-		this.players.push(recentPlayer); 
+		var playerInfo = new InitializePlayers(this.players,this.direction);
+
+		if (playerInfo.direction) {
+			playerInfo.currentPlayer = this.players[getElementIndex(this.players,this.currentPlayer,true)];
+			playerInfo.nextPlayer = this.players[getElementIndex(this.players,this.nextPlayer,true)];
+			playerInfo.previousPlayer = this.players[getElementIndex(this.players,this.previousPlayer,true)];
+		}else{
+			playerInfo.currentPlayer = this.players[getElementIndex(this.players,this.currentPlayer,false)];
+			playerInfo.nextPlayer = this.players[getElementIndex(this.players,this.nextPlayer,false)];
+			playerInfo.previousPlayer = this.players[getElementIndex(this.players,this.previousPlayer,false)];
+		};
+
+		return playerInfo;
 	},
 	changeDirection : function(){
-		var playersExceptCurrentPlayer = this.players.splice(1);
-		this.players = this.players.concat(playersExceptCurrentPlayer.reverse());
+		var playerInfo = new InitializePlayers(this.players,this.direction);
+
+		if (!this.direction) {
+			playerInfo.previousPlayer = this.nextPlayer;
+			playerInfo.nextPlayer = this.previousPlayer;
+		};
+		
+		return playerInfo;
 	}
 };
 
