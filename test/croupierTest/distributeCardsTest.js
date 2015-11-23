800a2163d1f49a7fe33ec596977bfdd177c89003
Arindam Karmakar <arindam@INarindam.local>
2015-11-23 10:34:46 +0530

    [Arindam & Gaurav] (distributeCardsTest.js) path is changed of requirement files & passed the tests

--- test/croupierTest/distributeCardsTest.js
+++ test/croupierTest/distributeCardsTest.js
@@ -1,4 +1,4 @@
-var distributeCards = require('../entities.js').distributeCards;
+var distributeCards = require('../../serverRequirement.js').server.distributeCards;
 var expect = require('chai').expect;
 
 //--------------------------------------------------------------------------------------------------------------//
