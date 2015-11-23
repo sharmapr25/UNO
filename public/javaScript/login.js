8e1522c7d9f487830797a86e795921a4b0c332c8
ganeshpl <ganespl@INganespl.local>
2015-11-23 15:43:27 +0530

    [Ganesh/Gaurav] login.html file added.. server.js file updated.. server handles the request for login page till 3 users..

---
+++ public/javaScript/login.js
@@ -0,0 +1,30 @@
+var alreadyGameStarted = function(){
+	return 'Sorry..!!! Game has already been started..!';
+};
+
+var playersInformation = function(comments){
+	return '<h4> Number of Players in The Game :'+comments.numberOfPlayers+'</h4>';
+}
+
+var sendConnectionRequest = function(){
+	var req = new XMLHttpRequest();
+	req.onreadystatechange = function() {
+	    if (req.readyState == 4 && req.status == 200) {
+	    	console.log(req.responseText);
+	        var comments = JSON.parse(req.responseText);
+	        console.log(comments);
+	        if(comments.isGameStarted == true){
+		      	document.querySelector('#current_game_information').innerHTML = alreadyGameStarted();
+	        }else{
+	        	document.querySelector('#current_game_information').innerHTML = playersInformation(comments);
+	        }
+	    };
+	};
+	req.open('POST', 'login_user', true);
+	req.send('name='+document.querySelector('input[name="user_name"]').value);
+	document.querySelector('input[name="user_name"]').value = "";
+};
+
+window.onload = function(){
+	document.querySelector('button').onclick = sendConnectionRequest;
+};
