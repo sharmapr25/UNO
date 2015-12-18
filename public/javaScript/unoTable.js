c6c9f017b0b81679bd6845b07582be23887d947c
ganeshpl <ganespl@INganespl.local>
2015-12-18 09:37:26 +0530

    [Ganesh] - card generator moved to client side
    What changes have you made?
      changes made in unotable.js to generate the cards at client side
    
    server side card generator is still there..havent removed..
    *
    
    Why do we need this change?
      to reduce the network traffic

--- public/javaScript/unoTable.js
+++ public/javaScript/unoTable.js
@@ -195,10 +195,11 @@ var sendConnectionRequest = function(){
 	    	for(var i=0; i < comments.userCards.length; i++){
 	    		var num = comments.userCards[i].number ? ' '+comments.userCards[i].number : '#';
 	    		var colour = comments.userCards[i].colour ? comments.userCards[i].colour : 'gray'
-	    		// imgRef += '<div id="card_num:'+i+'" height="270px" width="200px" onclick="make_request_to_play_the_card(this.id)" >' + createCard(num,colour) + '</div>'
-	    		imgRef += '<img id="card_num:'+i+'" class="user_cards" onclick="make_request_to_play_the_card(this.id)" src="'+addressGenrator(comments.userCards[i])+'">';
+	    		imgRef += '<div id="card_num:'+i+'" height="270px" width="200px" class="user_cards" onclick="make_request_to_play_the_card(this.id)" >' + createCard(comments.userCards[i]) + '</div>'
+	    		// imgRef += '<img id="card_num:'+i+'" class="user_cards" onclick="make_request_to_play_the_card(this.id)" src="'+addressGenrator(comments.userCards[i])+'">';
 	    	};			
 
+        document.getElementById('table_deck').className = comments.runningColour;
 		  	document.getElementById('cards').innerHTML = imgRef;
 			document.getElementById('message_box').innerHTML = generateMessage(comments);
 
@@ -215,26 +216,45 @@ var sendConnectionRequest = function(){
 };
 
 
-var createCard = function(num, colour){
-		colour = colour.toLowerCase();
+var createCard = function(card){
+	var num;
+	var colour = card.colour ? card.colour : '#ffffff'
 
-		return [
-		'<svg width="187" height="262" xmlns="http://www.w3.org/2000/svg">',
-		 '<g>',
-		  '<title>background</title>',
-		  '<rect fill="#fff" id="canvas_background" height="264" width="189" y="-1" x="-1"/>',
-		  '<g display="none" overflow="visible" y="0" x="0" height="100%" width="100%" id="canvasGrid">',
-		   '<rect fill="url(#gridpattern)" stroke-width="0" y="0" x="0" height="100%" width="100%"/>',
-		  '</g>',
-		 '</g>',
-		 '<g>',
-		  '<title>Layer 1</title>',
-		  '<rect id="svg_1" height="224" width="152" y="22" x="17.5" stroke-width="1.5" stroke="#000" fill="'+colour+'"/>',
-		  '<text stroke="#000" transform="matrix(4.747072696685791,0,0,3.642857074737549,-256.6744797229767,-216.714280128479) " xml:space="preserve" text-anchor="left" font-family="Helvetica, Arial, sans-serif" font-size="24" id="svg_3" y="104" x="66" stroke-width="0" fill="#000000"/>',
-		  '<text xml:space="preserve" text-anchor="left" font-family="Helvetica, Arial, sans-serif" font-size="111" id="svg_4" y="166" x="27" fill-opacity="null" stroke-opacity="null" stroke-width="0" stroke="#000" fill="#000000">'+num+'</text>',
-		 '</g>',
-		'</svg>'
-		].join('');
+	if(card.number || card.number == 0){
+		num = ' ' + card.number;
+	}else{
+		switch(card.speciality){
+			case 'Reverse':
+				num = ' R';break;
+			case 'Skip':
+				num = '@';break;
+			case 'DrawTwo':
+				num = '+2';break;
+			case 'Wild':
+				num = 'W';break;
+			case 'WildDrawFour':
+				num = '+4';break;
+		};
+	};
+
+	colour = colour.toLowerCase();
+	return [
+	'<svg width="187" height="262" xmlns="http://www.w3.org/2000/svg">',
+	 '<g>',
+	  '<title>background</title>',
+	  '<rect fill="#fff" id="canvas_background" height="264" width="189" y="-1" x="-1"/>',
+	  '<g display="none" overflow="visible" y="0" x="0" height="100%" width="100%" id="canvasGrid">',
+	   '<rect fill="url(#gridpattern)" stroke-width="0" y="0" x="0" height="100%" width="100%"/>',
+	  '</g>',
+	 '</g>',
+	 '<g>',
+	  '<title>Layer 1</title>',
+	  '<rect id="svg_1" height="224" width="152" y="22" x="17.5" stroke-width="1.5" stroke="#000" fill="'+colour+'"/>',
+	  '<text stroke="#000" transform="matrix(4.747072696685791,0,0,3.642857074737549,-256.6744797229767,-216.714280128479) " xml:space="preserve" text-anchor="left" font-family="Helvetica, Arial, sans-serif" font-size="24" id="svg_3" y="104" x="66" stroke-width="0" fill="#000000"/>',
+	  '<text xml:space="preserve" text-anchor="left" font-family="sans-serif" font-size="111" id="svg_4" y="166" x="27" fill-opacity="null" stroke-opacity="null" stroke-width="0" stroke="#000" fill="#000000">'+num+'</text>',
+	 '</g>',
+	'</svg>'
+	].join('');
 };
 
 var interval = setInterval(sendConnectionRequest, 500);
