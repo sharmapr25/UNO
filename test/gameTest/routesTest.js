c0535e25ac339b7a78e1d4dd194b994ed0465324
ganeshpl <ganespl@INganespl.local>
2015-12-24 13:19:46 +0530

    [Preeti / Ganesh] - test for POST methos
    What changes have you made?
      added the test for all the methods that server can handle and also for
    invalid methods
    
    Why do we need this change?
      to make sure that everything is working while refractoring the code
    
    Files Modified:

--- test/gameTest/routesTest.js
+++ test/gameTest/routesTest.js
@@ -79,9 +79,197 @@ describe('Routes', function(){
     
     describe('/play_card', function(){
       it('plays the card if the card that the player plays is allowed', function(done){
-        var play_card_info = {playedCard : {colour : 'Red', number : 10, speciality : null, points : 10}};  
-        done();
+        var play_card_info = {playedCard : {colour : 'Red', number : '5', speciality : null, points : 1}};  
+        var game = {
+          no_of_players : 1,
+          usersInformation : [{name : 'Ram'}],
+          user_cards : {'Ram' : [{number : '5', colour : 'Red', speciality : null, points : 1}]},
+          isGameStarted : true,
+          runningColour : 'Red',
+          plus_two_cards_count : 0,
+          discard_pile : { 
+            getTopMostCard : function(){ 
+              return {number : '3', colour : 'Red', speciality : null, points : 1}
+            },
+            addCard : function(card){}
+          },
+          players : { changePlayersTurn : function(){}},
+          currentPlayer : 'Ram',
+          said_UNO_registry : []
+        };      
+         
+        request(controller(game))
+          .post('/public/htmlFiles/play_card')
+          .set('Cookie', 'Ram')
+          .send(play_card_info)
+          .expect(/successful/)
+          .expect(200, done);
+      });
+
+      it('should notify the user about invalid_card if the player plays the card that is not valid', function(done){
+        var play_card_info = {playedCard : {colour : 'Red', number : '5', speciality : null, points : 1}};  
+        var game = {
+          no_of_players : 1,
+          usersInformation : [{name : 'Ram'}],
+          user_cards : {'Ram' : [{number : '5', colour : 'Red', speciality : null, points : 1}]},
+          isGameStarted : true,
+          runningColour : 'Red',
+          plus_two_cards_count : 0,
+          discard_pile : { 
+            getTopMostCard : function(){ 
+              return {number : '3', colour : 'Blue', speciality : null, points : 1}
+            },
+            addCard : function(card){}
+          },
+          currentPlayer : 'Ram'
+        };      
+         
+        request(controller(game))
+          .post('/public/htmlFiles/play_card')
+          .set('Cookie', 'Ram')
+          .send(play_card_info)
+          .expect(/can_not_play_the_card/)
+          .expect(200, done);
+      });
+
+      it('should not allow the user to play the card if its not his turn', function(done){
+        var play_card_info = {playedCard : {colour : 'Red', number : '5', speciality : null, points : 1}};  
+        var game = {
+          no_of_players : 2,
+          usersInformation : [{name : 'Ram'}, {name : 'Shyam'}],
+          user_cards : { 'Ram' : [{number : '5', colour : 'Red', speciality : null, points : 5}],
+                         'Shyam' :  [{number : '6', colour : 'Red', speciality : null, points : 6}]
+
+                       },
+          currentPlayer : 'Shyam'
+        };      
+         
+        request(controller(game))
+          .post('/public/htmlFiles/play_card')
+          .set('Cookie', 'Ram')
+          .send(play_card_info)
+          .expect(/not_your_turn/)
+          .expect(200, done); 
       });
     });
+
+    describe('/DrawCard', function(){
+      it('should allow the user to draw a card if its his turn', function(done){ 
+        var game = {
+          currentPlayer : 'Ram',
+          draw_pile : { drawCards : function(num){
+                          return {number : '1', colour : 'Green', speciality : null, points : 1};
+                        },
+                        isEmpty : function(){return false;}
+                      } 
+        };
+
+        request(controller(game))
+          .post('/public/htmlFiles/draw_card')
+          .set('Cookie', 'Ram')
+          .send()
+          .expect(200, done); 
+      });
+
+      it('should not allow the user to draw a card if its not his turn', function(done){
+          var game = {
+             currentPlayer : 'Ram'
+          };
+
+        request(controller(game))
+          .post('/public/htmlFiles/draw_card')
+          .set('Cookie', 'Shyam')
+          .send()
+          .expect('not_your_turn')
+          .expect(200, done);  
+      });
+    });
+
+    describe('/public/htmlFiles/pass_turn', function(){
+      it('should allow the user to pass the turn to next player', function(done){
+          var game = {
+             players : { changePlayersTurn : function(){}}
+          };
+
+        request(controller(game))
+          .post('/public/htmlFiles/pass_turn')
+          .set('Cookie', 'Shyam')
+          .expect('turn_passed')
+          .expect(200, done);  
+      });
+    });
+  
+    describe('say_uno', function(){
+      it('should consider the user\'s said_uno request', function(done){
+        var game = {
+          said_UNO_registry : [ {name : 'Ram', said_uno : false},
+                                {name : 'Shyam', said_uno : false}
+                              ],
+        };
+
+        request(controller(game))
+          .post('/public/htmlFiles/say_uno')
+          .set('Cookie', 'Ram')
+          .expect('said_uno_sucessfully')
+          .expect(200, done);
+
+        //test whether the said uno field changed or not
+      });
+    });
+  
+    describe('/catch_uno', function(){
+      it('should tell the user that he catched uno if any user have 1 card and he didnt say uno', function(done){
+         var game = {
+          user_names : ['Ram', 'Shyam'],
+          user_cards : { Ram : [{}], Shyam : [{}, {}, {}] }, 
+          said_UNO_registry : [ {name : 'Ram', said_uno : false},
+                                {name : 'Shyam', said_uno : false}
+                              ],
+          draw_pile : { drawCards : function(num){ return [ {}, {}]},
+                        isEmpty : function(){ return false;}
+                      }
+        };
+
+
+        request(controller(game))
+          .post('/public/htmlFiles/catch_uno')
+          .set('Cookie', 'Shyam')
+          .expect('uno_catched_successfully')
+          .expect(200, done);
+
+        //check whether 2 cards have been added to the player which didnt say uno
+
+      });      
+    
+      it('should tell the user that no one to catch if the user has said uno', function(done){
+          var game = {
+          user_names : ['Ram', 'Shyam'],
+          user_cards : { Ram : [{}], Shyam : [{}, {}, {}] }, 
+          said_UNO_registry : [ {name : 'Ram', said_uno : true},
+                                {name : 'Shyam', said_uno : false}
+                              ],
+          draw_pile : { drawCards : function(num){ return [ {}, {}]},
+                        isEmpty : function(){ return false;}
+                      }
+        };
+
+
+        request(controller(game))
+          .post('/public/htmlFiles/catch_uno')
+          .set('Cookie', 'Shyam')
+          .expect('no_one_to_catch_uno')
+          .expect(200, done); 
+      });
+    });
+    describe('Invalid URL', function(){
+      it('should notify the user that the invalid method is not allowed', function(done){
+          var game = {};
+          request(controller(game))
+            .post('/hello')
+            .expect('Method NOT allowed..!!')
+            .expect(405, done);
+      
+      });
+     });
   });
 });
