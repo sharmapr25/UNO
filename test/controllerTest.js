de8181da0d63c389e4ec339098ec41696d7b3bc4
spreeti <spreeti@INspreeti.local>
2016-01-14 14:12:20 +0530

    [gaurav/preeti] Removed unneccessary test

--- test/controllerTest.js
+++ test/controllerTest.js
@@ -11,20 +11,4 @@ describe('/',function(){
 	});
 });
 
-describe('/public/css/login.css',function(){
-	it('should gives css for login page',function(done){
-		request(controller)
-		.get('/public/css/login.css')
-		.expect('location','/css/login.css')
-		.expect(302,done)
-	});
-});
 
-describe('/public/javaScript/login.js',function(){
-	it('should gives javaScript for login page',function(done){
-		request(controller)
-		.get('/public/javaScript/login.js')
-		.expect('location','/javaScript/login.js')
-		.expect(302,done)
-	});
-});
