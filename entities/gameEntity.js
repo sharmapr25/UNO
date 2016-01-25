d0f7d758fcc256cab387981dd408519674938f9c
spreeti <spreeti@INspreeti.local>
2016-01-25 16:15:59 +0530

    [preeti/gaurav] fix bugs of drawFourCard
    What changes have you made?
    
    * fix bug of drawFourCard in js file
    
    Why do we need this change?
    
    * To prevent player to play the drawFourCard if player have card to play
    
    What can this change impact?
    
    * It make more realiable and useful game

--- entities/gameEntity.js
+++ entities/gameEntity.js
@@ -1,15 +1,15 @@
 exports.game = function(id,numOfPlayer) {
   this.id = id;
   this.no_of_players = numOfPlayer,
-  this.usersInformation = [],//!array of players url
+  this.usersInformation = [],
   this.user_names = undefined;
   this.user_cards = [],
   this.discard_pile = undefined;
   this.draw_pile = undefined;
   this.isGameStarted = false,
-  this.players = undefined;//!player entity -current,next,prev   -> playersAttirbutes
+  this.players = undefined;
   this.runningColour = undefined; 
-  this.currentPlayer = undefined; // ?why are you define here currentplayer ?why it needed ?then where is other attribute
-  this.plus_two_cards_count = 0, //? why we needed it 
-  this.said_UNO_registry = [] //?why we needed 
+  this.currentPlayer = undefined;
+  this.plus_two_cards_count = 0, 
+  this.said_UNO_registry = [] 
 };
